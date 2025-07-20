6d92f29b8f9b053f673db9908a68448a0e4d9037
brucekaushik <kaushik.srinivas@gmail.com>
2025-07-20 15:55:45 +0530

    refactor: update app orchestration to reflect stage-based packing more accurately

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ from cccp.codec.packers import AsciiToJsonIr, JsonIrToBin
 if __name__ == "__main__":
 
     input_filepath = 'input_output/input2.txt'
-    packer = AsciiToJsonIr(input_filepath)
+    ir_packer = AsciiToJsonIr(input_filepath)
 
     output_filepath = 'input_output/input2_ir1.json'
-    packer.add_header("Knolbay:Poc1@1.0.0")
-    packer.encode()
-    packer.write_ir_to_file(output_filepath)
+    ir_packer.add_header("Knolbay:Poc1@1.0.0")
+    ir_packer.encode()
+    ir_packer.write_ir_to_file(output_filepath)
+    ir = ir_packer.get_ir()
+
+    bin_filepath = "input_output/input2_bin1.cccp"
+    bin_packer = JsonIrToBin()
+    bin_packer.set_ir(ir)
+    bin_packer.encode_and_write(bin_filepath)
 
     output_filepath = 'input_output/input2_ir2.json'
-    packer.add_header("Knolbay:Poc2@1.0.0")
-    packer.encode()
-    packer.write_ir_to_file(output_filepath)
-    ir = packer.get_ir()
+    ir_packer.add_header("Knolbay:Poc2@1.0.0")
+    ir_packer.encode()
+    ir_packer.write_ir_to_file(output_filepath)
+    ir = ir_packer.get_ir()
 
-    bin_filepath = "input_output/input2.bin"
-    packer = JsonIrToBin()
-    packer.set_ir(ir)
-    packer.encode_and_write(bin_filepath)
+    bin_filepath = "input_output/input2_bin2.cccp"
+    bin_packer = JsonIrToBin()
+    bin_packer.set_ir(ir)
+    bin_packer.encode_and_write(bin_filepath)
